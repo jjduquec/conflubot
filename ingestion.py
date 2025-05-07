5948e4c10fc8f26a05bc6b5e9c0897cf7eafb523
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2025-05-07 09:04:24 -0500

    added get_repository method

--- ingestion.py
+++ ingestion.py
@@ -6,7 +6,7 @@ from llama_index.core import Document,VectorStoreIndex,Settings,StorageContext
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-url="https://confluence.atlassian.com/jirasoftwareserver/getting-started-as-a-jira-software-user-938845161.html"
+
 
 embed_model=HuggingFaceEmbedding(model_name='./all-MiniLM-L12-v2')
 
@@ -15,6 +15,26 @@ Settings.embed_model=embed_model
 Settings.chunk_size=256
 Settings.chunk_overlap=20
 
+def save_content(context):
+    #prepare DB 
+    db=chromadb.PersistentClient(path="./db")
+    collection=db.get_or_create_collection("confluence")
+    vector_store=ChromaVectorStore(chroma_collection=collection)
+    storage_context=StorageContext.from_defaults(vector_store=vector_store)
+
+    #counting before store 
+    current_registries=collection.count()
+
+    #indexing  
+    document=Document(text=context)
+    VectorStoreIndex.from_documents([document],
+                                    storage_context=storage_context
+                                    )
+    
+    if(collection.count()>current_registries):
+        return True
+    else:  
+        raise ValueError('Context were not saved , please try again ')
 
 #extraer contenido repositorio confluence
 def get_content(url): 
@@ -35,3 +55,10 @@ def get_content(url):
         else:  
             raise ValueError('Unable to access to confluence repository')
 
+def get_repository(url):
+    try:
+        context=get_content(url)
+        if(save_content(context)):
+            return "Repository downloaded sucessfully"
+    except Exception as e :  
+        return e 
