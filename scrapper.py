d77cbbfdcea1e1653f9a0b6c665ec875f8ca83f1
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2025-05-12 18:48:08 -0500

    extract content

--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,45 @@
-import requests  
+import re
 from bs4 import BeautifulSoup
+from llama_index.core import Document  
 
-url="https://confluence.atlassian.com/jirasoftwareserver/using-jira-applications-with-advanced-roadmaps-for-jira-939938820.html"
-repository=requests.get(url) 
-soup=BeautifulSoup(repository.content,'html.parser')
-main_content=soup.find('div',class_='wiki-content')
+file=open('jira.html')
 
-#trying to find first H2  
-if main_content.find('h2') != None:  
-    sections=main_content.find_all('h2')
-    test=sections[0] 
-    section_content=test.find_all('p')
-    print(section_content)
+soup=BeautifulSoup(file,'html.parser')
+title=soup.find('title')
+title=title.text
+
+
+main_content=soup.find('div',class_='wiki-content') 
+#getting all pharaghraps  
+paragraphs=main_content.find_all('p')
+context=[]
+for p in paragraphs:  
+    #extracting content
+    if p.text != '':
+        content=re.sub(r'[^a-zA-Z0-9]', ' ', p.text)
+        topic=p.find_previous_sibling(['h2','h3','h4'])
+        #getting topic based on page titles  
+        if topic !=None: 
+            topic=topic.text 
+        else:  
+            topic='None'
+        
+        #looking for lists 
+        nested_list=p.find_next_sibling(['ol','ul'])
+        if nested_list !=None:  
+            nested_list=re.sub(r'[^a-zA-Z0-9]', ' ', nested_list.text)
+            content+='\n'+nested_list
+        document=Document(text=content,metadata={'title':title,'topic':topic})
+        context.append(document)
+
+
+
+
+
+       
+
+
+
+
+
+
