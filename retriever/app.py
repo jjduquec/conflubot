2a22a175852579ece862b960ffa73c37367ed11c
Juan Jose Duque Cardona <47718782+jjduquec@users.noreply.github.com>
2025-05-30 05:34:57 -0500

    divided the retriever app

---
+++ retriever/app.py
@@ -0,0 +1,15 @@
+from flask import Flask ,jsonify ,request 
+from retriever import ask_question
+
+app= Flask(__name__)
+
+@app.route('/api/ask_question/',methods=['GET'])
+def get_link():
+    json=request.get_json(force=True)
+    question=json.get('question')
+    message=ask_question(question)
+    return jsonify({'message':message})
+
+
+if __name__=='__main__':
+    app.run(host='127.0.0.1',port=8082)
